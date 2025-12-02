93c1d20e9f72d95b1a16e18106e1023095024a4d
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-02 17:18:33 +0100

    chore(database) : add DELETE script into animal and habitat tables

--- Database/zoo.sql
+++ Database/zoo.sql
@@ -27,10 +27,10 @@ VALUES
 --insert data into animal
 INSERT INTO animal(animal_name, animal_type, animal_img, habitat_ID)
 VALUES 
-('Lion', 'Carnivore', 'lion.jpg', 1),
-('Elephant', 'Herbivore', 'elephant.jpg', 1),
-('Tiger', 'Carnivore', 'tiger.jpg', 2),
-('Camel', 'Herbivore', 'camel.jpg', 3);
+('Lion', 'Carnivore', 'https://i.pinimg.com/1200x/b1/8b/5c/b18b5cafebed0f6005142924862c1323.jpg', 1),
+('Elephant', 'Herbivore', 'https://i.pinimg.com/1200x/31/d8/fc/31d8fc559d3a6f7a58be1b5d5fd52e0e.jpg', 1),
+('Tiger', 'Carnivore', 'https://i.pinimg.com/736x/02/fe/5c/02fe5cb3807f5acf315d363fb7f02c29.jpg', 2),
+('Camel', 'Herbivore', 'https://i.pinimg.com/1200x/04/f3/47/04f3476ca38806ed63c887c7ee5d5247.jpg', 3);
 
 -- Edit habitat 
 UPDATE habitat
@@ -45,3 +45,12 @@ SET animal_name = 'African Lion',
     animal_img = 'african_lion.jpg',
     habitat_ID = 1
 WHERE ID = 1;
+
+-- Delete habitat
+DELETE FROM habitat
+WHERE habitat_ID = 4; 
+
+
+-- Delete animal
+DELETE FROM animal
+WHERE ID = 4;  
