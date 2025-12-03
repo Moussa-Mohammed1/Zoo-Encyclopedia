d64d54021f7830246bae25e7c48f90bd8b3ead86
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-04 00:45:12 +0100

    feat : initialize habitats folder with ADD HABITAT

---
+++ habitats/add_habitat.php
@@ -0,0 +1,19 @@
+<?php 
+    include("./../config.php");
+
+    if(isset($_POST['submitHabitat'])){
+        $name = mysqli_real_escape_string($conn, $_POST['nom_habitat']);
+        $desc = mysqli_real_escape_string($conn, $_POST['description_hab']);
+
+        $sql = "INSERT INTO habitat (habitat_name, habitat_desc) 
+                VALUES ('$name','$desc')";
+        if (mysqli_query($conn, $sql)) {
+            echo "<script>alert('habitat saved')</script>;";
+        }
+        else{
+            echo "<script>alert('not saved !')</script>";
+        }
+    }
+    header('Location: ./../index.php');
+    exit();
+?>
