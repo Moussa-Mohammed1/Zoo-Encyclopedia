51cd3aa5d313d7b251170001512cf4aa7e305d19
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-04 13:14:56 +0100

    feat(CRUD): add delete action on animals

--- animals/delete_animal.php
+++ animals/delete_animal.php
@@ -1,3 +1,21 @@
 <?php 
-    include("./config.php");
+    include("./../config.php");
+
+    $id = $_GET['id'];
+    if ($id) {
+        $sql = "DELETE FROM animal
+                WHERE id = '$id'";
+        if (mysqli_query($conn, $sql)) {
+            header("Location: ./../index.php");
+            exit();
+        }
+        else{
+            header("Location: ./../index.php");
+            exit();
+        }
+    }
+    else{
+        header("Location: ./../index.php");
+        exit();
+    }
 ?>
