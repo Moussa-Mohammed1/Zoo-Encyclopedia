2010afda79be9005e299118522ee9c7dca6b20dc
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-04 13:10:52 +0100

    feat : handle modals appearance on CANCEL and OUTSIDE MODAL CLICKS

--- js/script.js
+++ js/script.js
@@ -21,3 +21,30 @@ function showAdmin(type) {
         animalBtn.className = "tab-btn bg-gray-200 text-gray-700 px-4 py-2 rounded-t-lg font-semibold transition hover:bg-gray-300";
     }
 }
+ 
+let habitatForm = document.getElementById('habitat-form');
+let cancel = document.getElementById('cancel');
+let addHabitat = document.getElementById('btn-ajouter-habitat');
+
+function handleCancel(){
+    location.href = "index.php";
+}
+
+addHabitat.addEventListener('click',()=>{
+    if (habitatForm.classList.contains('hidden')){
+        habitatForm.classList.remove('hidden');
+    }
+});
+cancel.addEventListener('click',handleCancel);
+
+let animalForm = document.getElementById('animal-form');
+let addAnimal = document.getElementById('btn-ajouter-animal');
+addAnimal.addEventListener('click',()=>{
+    if(animalForm.classList.contains('hidden'))animalForm.classList.remove('hidden');
+});
+animalForm.addEventListener('click', (e)=>{
+    if(!animalForm.firstElementChild.contains(e.target)){
+        animalForm.classList.add('hidden');
+    }
+});
+
