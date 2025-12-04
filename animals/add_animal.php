03f73e2559ef1c2e88b06275ac3eafacb19bc5a2
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2025-12-04 13:14:13 +0100

    feat(CRUD) : add create action on animals

--- animals/add_animal.php
+++ animals/add_animal.php
@@ -1,3 +1,18 @@
 <?php 
-    include("./config.php");
+    include("./../config.php");
+
+    if(isset($_POST['submitAnimal'])){
+        $name = mysqli_real_escape_string($conn, $_POST['animal_name']);
+        $type = mysqli_real_escape_string($conn, $_POST['animal_type']);
+        $img = mysqli_real_escape_string($conn, $_POST['animal_img']);
+        $habitat = mysqli_real_escape_string($conn, $_POST['habitat_ID']);
+
+        $sql = "INSERT INTO animal (animal_name, animal_type, animal_img, habitat_ID) 
+                VALUES ('$name', '$type', '$img', '$habitat')";
+        if (mysqli_query($conn, $sql)) {
+            echo "<script>alert('animal saved!')</script>";
+        }
+    }
+    header("Location: ./../index.php");
+    exit();
 ?>
